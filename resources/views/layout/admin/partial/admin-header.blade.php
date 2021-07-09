fac9cccf3c537b0b9d745fa0674ea86f62359907
Rio Martin Gojar <limlimgo14@gmail.com>
2021-07-09 08:52:50 +0800

    Update Webpack

--- resources/views/layout/admin/partial/admin-header.blade.php
+++ resources/views/layout/admin/partial/admin-header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-default navbar-fixed-top">
 			<div class="brand">
-				<a href="{{route('homePage')}}"><img src="{{asset('assets/img/logo-dark.png')}}" alt="Klorofil Logo" class="img-responsive logo"></a>
+				<a href="{{route('homePage')}}"><img src="{{asset('img/logo.png')}}" alt="Klorofil Logo" class="img-responsive logo" width="200"></a>
 			</div>
 			<div class="container-fluid">
 				<div class="navbar-btn">
@@ -17,7 +17,7 @@
 					<ul class="nav navbar-nav navbar-right">
 
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{asset('assets/img/user.png')}}" class="img-circle" alt="Avatar"> <span>{{Auth::user()->name}}</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{asset('img/user.png')}}" class="img-circle" alt="Avatar"> <span>{{Auth::user()->name}}</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
 							<ul class="dropdown-menu">
 								<li> <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
